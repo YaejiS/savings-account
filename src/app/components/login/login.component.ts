143d17719d75cc67d87dc4d1700cd04ba9658997
YaejiS <yaejishin@gmail.com>
2023-08-21 22:27:52 -0400

    update login  component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,26 +13,13 @@ export class LoginComponent {
   passwordValue: string = "";
 
   listener(event: any) {
-    // console.log(this.passwordInput.nativeElement.value)
     this.passwordValue = this.passwordInput.nativeElement.value;
 
     this.maskedPassword = this.passwordMask(this.passwordValue);
-
-    // console.log(event.key)
-    // if (event.key === 'Backspace') {
-    //   this.passwordValue = this.passwordValue.substr(0,this.passwordValue.length-1)
-    // } else if (event.key === 'Meta') {
-    //   // do nothing
-    // } else {
-    //   this.passwordValue += event.key;
-    // }
   }
 
   passwordMask(password: string) {
-    // return password.substring(0, password.length - 1) + '*';
-    // return password.substring(0, password.length - 1) + '*';
-    // let mask = password.replaceAll(/./, '*');
-    // return mask; // + password.substring(password.length - 1, password.length);
+
     let stupid = ''
     for (let i = 0; i < password.length - 1; i++) {
       stupid += '*'
