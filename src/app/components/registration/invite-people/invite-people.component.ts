2d0d665c405a93a6880261de64636d9150d4cf9f
YaejiS <yaejishin@gmail.com>
2023-08-21 22:35:01 -0400

    update invite-people component

--- src/app/components/registration/invite-people/invite-people.component.ts
+++ src/app/components/registration/invite-people/invite-people.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { RegistrationService } from 'src/app/services/registration.service';
-
+import {Observable, map, startWith} from 'rxjs';
 
 @Component({
   selector: 'app-invite-people',
@@ -8,6 +9,10 @@ import { RegistrationService } from 'src/app/services/registration.service';
   styleUrls: ['./invite-people.component.css']
 })
 export class InvitePeopleComponent {
+  
+  myControl = new FormControl('');
+  names: string[] = ['@Amy', '@Jared', '@Sally'];
+  filteredNames: Observable<string[]> | undefined;
 
   @Output() updatePageEvent = new EventEmitter<string>();
 
@@ -15,6 +20,19 @@ export class InvitePeopleComponent {
 
   updatePage( endpoint : string ) {
     this.service.updatePage( this.updatePageEvent, endpoint );
+  }
+
+  ngOnInit() {
+    this.filteredNames = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.names.filter(name => name.toLowerCase().includes(filterValue));
   }
 
 }
