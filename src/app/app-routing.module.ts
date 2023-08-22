51c1a76c694dd9f8113d6acab73c170da9a7594f
YaejiS <yaejishin@gmail.com>
2023-08-21 21:28:39 -0400

    update module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LinkBankAccountComponent } from './components/registration/link-bank-account/link-bank-account.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './components/login/login.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatListModule } from '@angular/material/list';
 import { InvitePeopleComponent } from './components/registration/invite-people/invite-people.component';
 import { SetGoalComponent } from './components/registration/set-goal/set-goal.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDialog, MatDialogRef, MatDialogModule} from '@angular/material/dialog'
-import {MatIconModule} from '@angular/material/icon';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
 import { SetGoalComponent2 } from './components/registration/set-goal-2/set-goal-2.component';
+import { StatusComponent } from './components/registration/status/status.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -22,11 +14,12 @@ const routes: Routes = [
   { path: 'link-account', component: LinkBankAccountComponent },
   { path: 'invite-people', component: InvitePeopleComponent },
   { path: 'set-goal', component: SetGoalComponent },
-  { path: 'set-goal-2', component: SetGoalComponent2}
+  { path: 'set-goal-2', component: SetGoalComponent2},
+  { path: 'status', component: StatusComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), MatTabsModule, MatListModule, MatFormFieldModule, MatInputModule, BrowserAnimationsModule, MatDialogModule, MatIconModule, MatDatepickerModule, MatNativeDateModule],
-  exports: [RouterModule, MatTabsModule, MatListModule,MatFormFieldModule, MatInputModule, BrowserAnimationsModule, MatDialogModule, MatIconModule, MatDatepickerModule, MatNativeDateModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +6,25 @@ import { RegistrationComponent } from './components/registration/registration.co
 import { LinkBankAccountComponent } from './components/registration/link-bank-account/link-bank-account.component';
 import { InvitePeopleComponent } from './components/registration/invite-people/invite-people.component';
 import { SetGoalComponent } from './components/registration/set-goal/set-goal.component';
+import { SetGoalComponent2 } from './components/registration/set-goal-2/set-goal-2.component';
+import { StatusComponent } from './components/registration/status/status.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { CurrencyMaskModule } from "ng2-currency-mask";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
 
 @NgModule({
   declarations: [
@@ -16,13 +33,30 @@ import { SetGoalComponent } from './components/registration/set-goal/set-goal.co
     RegistrationComponent,
     LinkBankAccountComponent,
     InvitePeopleComponent,
-    SetGoalComponent
+    SetGoalComponent,
+    SetGoalComponent2,
+    StatusComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule, 
+    FormsModule, 
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatTabsModule,
+    MatListModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    CurrencyMaskModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  
 })
 export class AppModule { }
